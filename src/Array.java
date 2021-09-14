16f6c720feefc430fb4b3fd9dd926a2cbd29709d
Mamadou Anne <mamadiankha@gmail.com>
2021-09-14 09:54:30 -0700

    second commit

--- src/Array.java
+++ src/Array.java
@@ -7,8 +7,17 @@ public class Array {
     }
     public void insert(int item){
         // if the Array is full resize it
-        // add the new item at the end
-        items[count++] = item;
+        if(items.length == count) {
+            //create a new Array (twice the size)
+            int[] newItems = new int[count * 2];
+            //copy all existing items
+            for(int i = 0; i < count; i++)
+                newItems[i] = items[i];
+            //set "items" to this new Array
+            items = newItems;
+            // add the new item at the end
+            items[count++] = item;
+        }
 
     }
     public void print(){
