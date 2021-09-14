64d805e1006a05c2abd5bd07ff7d5aa18146420c
Mamadou Anne <mamadiankha@gmail.com>
2021-09-14 11:14:03 -0700

    adding insert and remove method

--- src/Array.java
+++ src/Array.java
@@ -5,20 +5,30 @@ public class Array {
     public Array(int length){
         items = new int[length];
     }
-    public void insert(int item){
-        // if the Array is full resize it
-        if(items.length == count) {
+
+    public void insert(int item) {
+        //if the Array is full resize it
+        if (items.length == count) {
             //create a new Array (twice the size)
             int[] newItems = new int[count * 2];
             //copy all existing items
-            for(int i = 0; i < count; i++)
+            for (int i = 0; i < count; i++)
                 newItems[i] = items[i];
             //set "items" to this new Array
             items = newItems;
+        }
             // add the new item at the end
             items[count++] = item;
-        }
+    }
 
+    public void removeAt(int index){
+        //validate the index
+        if(index < 0 || index >= count)
+            throw new IllegalArgumentException();
+        //shift the items to the left to fill the hole
+        for(int i = index; i < count; i++)
+            items[i] = items[i + 1];
+        count--;
     }
     public void print(){
         for (int i = 0; i < count; i++)
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         Array numbers = new Array(3);
         numbers.insert(10);
         numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.removeAt(2);
         numbers.print();
     }
 }
